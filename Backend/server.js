64e0f0eb1a18dae7c4f71985c3545893279c80a5
santoshtuniki <tunikisantosh123@gmail.com>
2024-05-09 12:01:07 +0530

    MongoDB connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
+
 require('dotenv').config();
 require('colors');
+
+connectDB();
 
 const app = express();
 
@@ -18,6 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
+// For any undefined Api's
+app.get('*', (req, res) => {
+    res.status(404).send('Endpoint does not exist');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is connected in ${process.env.NODE_ENV} mode on port ${PORT}`.blue);
+    console.log(`Server is connected in ${process.env.NODE_ENV} mode on port ${PORT}`.red);
 });
